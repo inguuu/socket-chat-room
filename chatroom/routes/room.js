0c296bc558a3d451a1b6b1cfe974a7b4b7cf059f
inguuu <dlsrb8949@gmail.com>
2019-09-01 15:49:06 +0900

    setting

--- chatroom/routes/room.js
+++ chatroom/routes/room.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const Room = require('../schemas/room');
+
 router.post('/', async (req, res) => {
 
 
